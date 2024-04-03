c19dc8bf4f70ad0ff64eb2dbae8a9a70f3c64117
SangWon7242 <gotkdqja@gmail.com>
2024-04-03 12:28:41 +0900

    검색 결과에 따라 json형태로 데이터가 출력되게

--- app/page.js
+++ app/page.js
@@ -1,3 +1,44 @@
+"use client";
+import axios from "axios";
+import { useState } from "react";
+
 export default function Home() {
-  return <div>안녕</div>;
+  const ID_KEY = process.env.NEXT_PUBLIC_API_KEY;
+  const SECRET_KEY = process.env.NEXT_PUBLIC_API_SECRET_KEY;
+
+  const [data, setData] = useState(null);
+  const onClick = async () => {
+    try {
+      const response = await axios.get("/v1/search/news.json", {
+        params: {
+          query: "날씨",
+          display: 20,
+        },
+        headers: {
+          "X-Requested-With": "XMLHttpRequest",
+          "X-Naver-Client-Id": ID_KEY,
+          "X-Naver-Client-Secret": SECRET_KEY,
+        },
+      });
+      // resp.json(response.data.items);
+      setData(response.data);
+      // console.log(response);
+    } catch (e) {
+      console.error(`e : ${e}`);
+      // resp.status(500).send("Internal Server Error");
+    }
+  };
+
+  return (
+    <div>
+      <button onClick={onClick}>불러오기</button>
+      {data && (
+        <textarea
+          rows={7}
+          value={JSON.stringify(data, null, 2)}
+          readOnly={true}
+        />
+      )}
+    </div>
+  );
 }
