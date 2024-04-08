5310b6a9d341ad90032e2317e2dfb4f6a5dbd646
SangWon7242 <gotkdqja@gmail.com>
2024-04-08 12:43:09 +0900

    검색시 해당 키워드와 관련된 뉴스를 메뉴 형태로 보여주기

--- app/page.js
+++ app/page.js
@@ -6,12 +6,14 @@ export default function Home() {
   const ID_KEY = process.env.NEXT_PUBLIC_API_KEY;
   const SECRET_KEY = process.env.NEXT_PUBLIC_API_SECRET_KEY;
 
-  const [data, setData] = useState(null);
+  const [data, setData] = useState([]);
+  const [searchData, setSerchData] = useState("");
+
   const onClick = async () => {
     try {
       const response = await axios.get("/v1/search/news.json", {
         params: {
-          query: "날씨",
+          query: searchData,
           display: 20,
         },
         headers: {
@@ -28,8 +30,30 @@ export default function Home() {
 
   return (
     <div>
-      <button onClick={onClick}>불러오기</button>
+      <div className="flex gap-x-3">
+        <input
+          className="input input-bordered"
+          type="text"
+          placeholder="날씨를 검색해주세요."
+          value={searchData}
+          onChange={(e) => setSerchData(e.target.value)}
+        />
+        <button className="btn btn-primary" onClick={onClick}>
+          검색
+        </button>
+      </div>
       {JSON.stringify(data, null, 2)}
+
+      <nav className="news-list-box">
+        <ul>
+          {Array.isArray(data.items) &&
+            data.items.map((item) => (
+              <li>
+                <a href="{item.originallink}">뉴스제목 : {item.title}</a>
+              </li>
+            ))}
+        </ul>
+      </nav>
     </div>
   );
 }
